813839c0e65891669caf8e232b976b4837e088ad
Hadesy2k <kelvinkstz@gmail.com>
2017-05-07 17:35:16 +0800

    header for header.php

--- watthub/header.php
+++ watthub/header.php
@@ -12,38 +12,94 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
+    <title> Watthub </title>
+
+    <link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+
+    <?php wp_head(); ?>
+
+    <style>
+        /* Header CSS */
+        .header {
+            background-color: #292c2f;
+            box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.15);
+            padding: 20px 40px;
+            height: 80px;
+            box-sizing: border-box;
+        }
+
+        .header .header-body {
+            max-width: 1200px;
+            text-align: center;
+            margin: 0 auto;
+        }
+
+        /* Logo */
+        .header .header-body h1 {
+            float: left;
+            font: normal 42px Cookie, Arial, Helvetica, sans-serif;
+            line-height: 40px;
+            margin: 0;
+        }
+
+        .header .header-body h1 a {
+            text-decoration: none;
+            color: #ffffff;
+        }
+
+        /* Logo message */
+        .header .header-body h1 span {
+            color: #5383d3;
+            font-size: 18px;
+        }
+
+        /* Menus */
+        .header .header-body nav {
+            font: 16px Arial, Helvetica, sans-serif;
+            line-height: 40px;
+            float: right;
+        }
+
+        .header .header-body nav a {
+            display: inline-block;
+            padding: 0 5px;
+            text-decoration: none;
+            color: #ffffff;
+            font-size: 16px;
+            opacity: 0.9;
+        }
+
+        .header .header-body nav a:hover {
+            opacity: 1;
+        }
+
+        .header .header-body nav a.selected {
+            color: #608bd2;
+            pointer-events: none;
+            opacity: 1;
+        }
+    </style>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'watthub' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
+    <header class="header">
+        <div class="header-body">
+            <h1><a href="#">Watthub</a></h1>
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'watthub' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+            <nav>
+                <a href="#"> Home </a>
+                <a href="#"> Blog </a>
+                <a href="#"> Order </a>
+                <a href="#"> About Us </a>
+            </nav>
+        </div>
+    </header>
 
 	<div id="content" class="site-content">
