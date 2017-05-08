e7b0f19aaa90699927161fe7d563a6411beb46f2
Hadesy2k <kelvinkstz@gmail.com>
2017-05-08 09:37:23 +0800

    footer added

--- wp-content/themes/watthub/footer.php
+++ wp-content/themes/watthub/footer.php
@@ -11,18 +11,78 @@
 
 ?>
 
-	</div><!-- #content -->
+	<style>
+        /* Footer CSS */
+        .footer {
+            background-color: #292c2f;
+            box-shadow: 1px 2px 2px rgba(0, 0, 0, 0.15);
+            padding: 20px 40px;
+            height: 300px;
+            box-sizing: border-box;
+        }
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'watthub' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'watthub' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'watthub' ), 'watthub', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+        .footer .footer-body {
+            max-width: 100%;
+            margin: 0 auto;
+        }
+
+        .footer .footer-body .footer-body-left {
+            float: left;
+            color: #ffffff;
+        }
+
+        .footer .footer-body .footer-body-left h1 {
+            font: normal 42px Cookie, Arial, Helvetica, sans-serif;
+            margin-bottom: 0px;
+        }
+
+        .footer .footer-body .footer-body-left h1 > span {
+            color: #5383d3;
+        }
+
+        .footer .footer-body .footer-body-left p {
+            font: normal 16px Arial, Helvetica, sans-serif;
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+
+        .footer .footer-body .footer-body-left a {
+            font: normal 14px Arial, Helvetica, sans-serif;
+            text-decoration: none;
+            color: #ffffff;
+        }
+
+        .footer .footer-body .footer-body-right {
+            padding-top: 35px;
+            float: right;
+            color: #ffffff;
+        }
+
+        .footer .footer-body .footer-body-right a {
+            font: normal 32px Cookie, Arial, Helvetica, sans-serif;
+            text-decoration: none;
+            color: #ffffff;
+            padding-right: 20px;
+        }
+    </style>
 
 <?php wp_footer(); ?>
+    <footer class="footer">
+        <div class="footer-body">
+            <div class="footer-body-left">
+                <h1>Watt<span>Hub</span></h1>
+                <p> &copy; WattHub 2017, Developed by WattHub Team </p>
+                <a href="mailto:support@watthub.io">support@watthub.io</a>
+            </div>
+
+            <div class="footer-body-right">
+                <a href="#" class="selected"> Home </a>
+                <a href="#"> Blog </a>
+                <a href="#"> Order </a>
+                <a href="#"> About Us </a>
+            </div>
+        </div>
+    </footer>
 
 </body>
 </html>
