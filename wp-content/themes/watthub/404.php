d2cda5a2d8c0a6f24dc055793ac76a2514b8f15c
Ghost <Hadesy2k@users.noreply.github.com>
2017-05-19 16:37:10 +0800

    404 custom message

--- wp-content/themes/watthub/404.php
+++ wp-content/themes/watthub/404.php
@@ -9,56 +9,17 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<style>
+		.notfound-body {
+			text-align: center;
+		}
+	</style>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'watthub' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'watthub' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( watthub_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'watthub' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'watthub' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	<div class="notfound-body">
+		<img src="http://khantsithuz.sgedu.site/a2-wp/wp-content/uploads/images/404.png" width="500px" height="500px"/>
+		<h1><b> I don't think you are looking for <font color="purple">Unicorn</font></b></h1>
+		<h5 style="color:red"> 404 NOT FOUND </h5>
+	</div>
 
 <?php
 get_footer();
